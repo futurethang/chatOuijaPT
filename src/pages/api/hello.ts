b633fc6179612a163cdded876c911a5a353b3072
Kevin Hyde <kphyde@gmail.com>
2023-04-01 17:40:08 -0700

    initial commit, add basic page and openAI api

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { Configuration, OpenAIApi } from 'openai'
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.listModels();
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   res.status(200).json({ name: 'John Doe' })
 }
+
+// export function gpt3(req, res) {
+//   const { query } = req.body
+//   const openai = new OpenAIApi(process.env.OPENAI_API_KEY)
+//   const prompt = 'This is a test of GPT-3. ' + query;
+//   const maxTokens = 5;
+//   const temperature = 0.9;
+//   const topP = 1;
+//   const presencePenalty = 0;
+//   const frequencyPenalty = 0;
+//   const bestOf = 1;
+//   const n = 1;
+//   const stream = false;
+// }
