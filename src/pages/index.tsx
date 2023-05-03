5edd7c88fa1164487601abc7aa406d3ce10bf683
Kevin Hyde <kphyde@gmail.com>
2023-05-02 19:14:43 -0700

    letter by letter render under stylus

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { ChangeEvent, FormEvent, useState } from 'react'
 import { Inter } from 'next/font/google'
+import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -8,13 +9,31 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   const [input, setInput] = useState('');
   const [output, setOutput] = useState('');
+  const [fullText, setFullText] = useState('');
+  const [showText, setShowText] = useState(false);
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     const response = await fetch(`/api/chat2?input=${input}`);
-    const data = await response.text();
-    setOutput(data);
+    const data = await response.json();
+    setFullText(data.content)
+    renderOneLetterAtATime(data.content);
   };
+
+  const renderOneLetterAtATime = (text: string) => {
+    const letters = text.split('');
+    let i = 0;
+    const interval = setInterval(() => {
+      if (i < letters.length) {
+        setOutput(letters[i]);
+        i++;
+      } else {
+        setShowText(true);
+        clearInterval(interval);
+      }
+    }, 1500);
+  };
+
 
   return (
     <>
@@ -23,22 +42,25 @@ export default function Home() {
         <meta name="description" content="Your conduit to probably what the Spirit Realm would say." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
+        <link href="https://fonts.googleapis.com/css2?family=Averia+Serif+Libre:wght@700&family=Cinzel+Decorative:wght@700&display=swap" rel="stylesheet" />
+
       </Head>
       <main className={styles.main}>
-        <h1>Simple App</h1>
-        <form onSubmit={handleSubmit}>
+        {/* <h1>Chat Ouija</h1> */}
+        <div className={styles.stylusDiv}>
+          <div className={styles.letter}>{output}</div>
+          <Image src='/images/stylus.png' alt="ouija stylus" width={100} height={100} className={styles.stylus} />
+          <div className="fullText">{showText ? fullText : ''}</div>
+        </div>
+        <form onSubmit={handleSubmit} style={{ textAlign: 'center' }}>
           <label>
-            Input:
             <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
           </label>
-          <button type="submit">Submit</button>
+          <br />
+          <button type="submit">Ask Sprit</button>
         </form>
-        {output && (
-          <div>
-            <h2>Output:</h2>
-            <p>{output}</p>
-          </div>
-        )}
       </main>
     </>
   )
