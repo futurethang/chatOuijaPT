ffb8c522cba4b0e67dc30b361364e0c903397f6d
Kevin Hyde <kphyde@gmail.com>
2023-05-05 13:04:57 -0700

    smooth out letter animation component and debug some errors

---
+++ src/components/LetterFade.tsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from 'react';
+import styles from '../styles/LetterFade.module.css';
+
+interface LetterFadeProps {
+    text: string;
+    delay?: number;
+}
+
+interface LetterState {
+    opacity: number;
+    display: boolean;
+}
+
+const LetterFade: React.FC<LetterFadeProps> = ({ text = '', delay = 500 }) => {
+    const [currentIndex, setCurrentIndex] = useState<number>(0);
+    const [lettersState, setLettersState] = useState<LetterState[]>(
+        Array.from({ length: text.length }, () => ({
+            opacity: 0,
+            display: false,
+        }))
+    );
+
+
+    useEffect(() => {
+        setCurrentIndex(0);
+        setLettersState(
+            Array.from({ length: text.length }, () => ({
+                opacity: 0,
+                display: false,
+            }))
+        );
+    }, [text]);
+
+    useEffect(() => {
+        if (currentIndex < text.length) {
+            const fadeInDuration = delay * 0.2;
+            const fadeOutDuration = delay * 0.2;
+            const stableDuration = delay - fadeInDuration - fadeOutDuration;
+
+            const fadeIn = setTimeout(() => {
+                setLettersState((prevState) => {
+                    const newState = [...prevState];
+                    if (!newState) {
+                        debugger;
+                    }
+                    newState[currentIndex]!.opacity = 1;
+                    newState[currentIndex]!.display = true;
+                    return newState;
+                });
+            }, 0);
+
+            const stable = setTimeout(() => {
+                setLettersState((prevState) => {
+                    const newState = [...prevState];
+                    newState[currentIndex].opacity = 1;
+                    return newState;
+                });
+            }, fadeInDuration);
+
+            const fadeOut = setTimeout(() => {
+                setLettersState((prevState) => {
+                    const newState = [...prevState];
+                    newState[currentIndex].opacity = 0;
+                    return newState;
+                });
+            }, fadeInDuration + stableDuration);
+
+            const nextLetter = setTimeout(() => {
+                setCurrentIndex(currentIndex + 1);
+            }, delay);
+
+            return () => {
+                clearTimeout(fadeIn);
+                clearTimeout(stable);
+                clearTimeout(fadeOut);
+                clearTimeout(nextLetter);
+            };
+        }
+    }, [currentIndex, text, delay]);
+
+    return (
+        <div>
+            {text.split('').map((letter, index) => (
+                <span
+                    key={index}
+                    className={styles.letter}
+                    style={{
+                        opacity: lettersState[index]?.opacity ?? 0,
+                        visibility: lettersState[index]?.display ? 'visible' : 'hidden',
+                    }}
+                >
+                    {letter}
+                </span>
+            ))}
+        </div>
+    );
+};
+
+export default LetterFade;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,19 @@
-import Head from 'next/head'
-import { ChangeEvent, FormEvent, useState } from 'react'
-import { Inter } from 'next/font/google'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import { FormEvent, useEffect, useRef, useState } from 'react';
+import Image from 'next/image';
+import styles from '@/styles/Home.module.css';
+import LetterFade from '@/components/LetterFade';
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+export default function App() {
   const [input, setInput] = useState('');
   const [output, setOutput] = useState('');
-  const [fullText, setFullText] = useState('');
-  const [showText, setShowText] = useState(false);
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     const response = await fetch(`/api/chat2?input=${input}`);
     const data = await response.json();
-    setFullText(data.content)
-    renderOneLetterAtATime(data.content);
+    setOutput(data.content);
   };
-
-  const renderOneLetterAtATime = (text: string) => {
-    const letters = text.split('');
-    let i = 0;
-    const interval = setInterval(() => {
-      if (i < letters.length) {
-        setOutput(letters[i]);
-        i++;
-      } else {
-        setShowText(true);
-        clearInterval(interval);
-      }
-    }, 1500);
-  };
-
 
   return (
     <>
@@ -42,17 +22,10 @@ export default function Home() {
         <meta name="description" content="Your conduit to probably what the Spirit Realm would say." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
-        <link href="https://fonts.googleapis.com/css2?family=Averia+Serif+Libre:wght@700&family=Cinzel+Decorative:wght@700&display=swap" rel="stylesheet" />
-
       </Head>
       <main className={styles.main}>
-        {/* <h1>Chat Ouija</h1> */}
         <div className={styles.stylusDiv}>
-          <div className={styles.letter}>{output}</div>
-          <Image src='/images/stylus.png' alt="ouija stylus" width={100} height={100} className={styles.stylus} />
-          <div className="fullText">{showText ? fullText : ''}</div>
+          {output ? <LetterFade text={output} delay={3000} /> : ''}
         </div>
         <form onSubmit={handleSubmit} style={{ textAlign: 'center' }}>
           <label>
@@ -63,5 +36,5 @@ export default function Home() {
         </form>
       </main>
     </>
-  )
+  );
 }
